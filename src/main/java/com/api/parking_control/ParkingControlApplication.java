6bda32fc79c775d2fb53091e08fce8b3ec1e9f2b
jhonatan <jhonatanlira@gmail.com>
2024-12-25 17:50:45 -0300

    Primeiro commit

---
+++ src/main/java/com/api/parking_control/ParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.parking_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+	//https://youtu.be/LXRU-Z36GEU?t=3349
+}
